733953abebf9cedbc7424dc04bded23f068636c3
vy vo <tuonggvy.vo@gmail.com>
2020-11-25 03:48:26 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,20 +1,22 @@
-# class Book
-#   attr_accessor :author, :page_count
-#   attr_reader :title, :genre
+class Book
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
-#   GENRES = []
+  GENRES = []
 
-#   def initialize(title)
-#     @title = title
-#   end
+  def initialize(title)
+    @title = title
+  end
 
-#   def turn_page
-#     puts "Flipping the page...wow, you read fast!"
-#   end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
   
-#   def genre=(genre)
-#     @genre = genre
-#     GENRES << genre 
-#   end
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
+  end
 
-# end
+end
+
+  
