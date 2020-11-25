08e85867493efc2cba227bbc185f8d9741530478
vy vo <tuonggvy.vo@gmail.com>
2020-11-25 03:57:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,19 +1,19 @@
-class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+# class Shoe
+#   attr_accessor :color, :size, :material, :condition
+#   attr_reader :brand
   
-  BRANDS = []
+#   BRANDS = []
 
-  def initialize(brand)
-    @brand = brand
-    if !(BRANDS.include? (brand))
-      BRANDS << brand 
-    end
-  end
+#   def initialize(brand)
+#     @brand = brand
+#     if !(BRANDS.include? (brand))
+#       BRANDS << brand 
+#     end
+#   end
 
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
-  end
+#   def cobble
+#     self.condition = "new"
+#     puts "Your shoe is as good as new!"
+#   end
     
-end
+# end
